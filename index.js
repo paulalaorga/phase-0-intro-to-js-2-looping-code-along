7215957e63442b3ac4a331a748c18de69719c8db
paula <paulalaorga@gmail.com>
2023-07-24 16:55:54 +1000

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"];
+
+function writeCards(names) {
+    const ThankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        ThankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return ThankYouMessages;
+}
+
+const messages = writeCards(names);
+console.log(messages);
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
